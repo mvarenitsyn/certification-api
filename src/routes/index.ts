7164bc2072b72032b633b92ffc8d542e78be9529
Mikhail V <m.varenitsyn@gmail.com>
2022-07-31 15:54:42 -0400

    fixed routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 
 import {certificates} from "./certificates";
+import {categories} from "./categories";
 
 export const apiRoutes = Router({})
 
 apiRoutes.use('/certificate', certificates)
-apiRoutes.use('/category', certificates)
+apiRoutes.use('/category', categories)
 
